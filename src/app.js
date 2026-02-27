4852a4edfde24581bd8268ea0356e7f9f83564a2
Chahat Yadav <160589578+Chahatyadav1@users.noreply.github.com>
2026-02-27 23:28:29 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,9 @@
 const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-
 const taskRoutes = require("./routes/task.routes");
 
 const app = express();
+
 app.use(express.json());
-
-app.use("/tasks", taskRoutes);
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.use("/api/tasks", taskRoutes);
 
 module.exports = app;
