3bf9b6c086126087f2f074933918683e224ce61d
Chahat Yadav <160589578+Chahatyadav1@users.noreply.github.com>
2026-02-27 23:32:50 +0530

    initial commit

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,26 +1,37 @@
-const Task = require("../models/task.model");
+const taskService = require("../services/task.service");
 
-exports.createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json(task);
+exports.createTask = async (req, res, next) => {
+  try {
+    const task = await taskService.createTask(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    next(error);
+  }
 };
 
-exports.getTasks = async (req, res) => {
-  const tasks = await Task.find();
-  res.json(tasks);
+exports.getTasks = async (req, res, next) => {
+  try {
+    const tasks = await taskService.getTasks();
+    res.status(200).json(tasks);
+  } catch (error) {
+    next(error);
+  }
 };
 
-exports.getTaskById = async (req, res) => {
-  const task = await Task.findById(req.params.id);
-  res.json(task);
+exports.updateTask = async (req, res, next) => {
+  try {
+    const task = await taskService.updateTask(req.params.id, req.body);
+    res.status(200).json(task);
+  } catch (error) {
+    next(error);
+  }
 };
 
-exports.updateTask = async (req, res) => {
-  const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(task);
-};
-
-exports.deleteTask = async (req, res) => {
-  await Task.findByIdAndDelete(req.params.id);
-  res.json({ message: "Deleted successfully" });
+exports.deleteTask = async (req, res, next) => {
+  try {
+    await taskService.deleteTask(req.params.id);
+    res.status(200).json({ message: "Task deleted" });
+  } catch (error) {
+    next(error);
+  }
 };
