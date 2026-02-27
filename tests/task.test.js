9dd1321adad6bb3162043accc200f40d276cd214
Chahat Yadav <160589578+Chahatyadav1@users.noreply.github.com>
2026-02-27 23:42:43 +0530

    Update task.test.js

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,5 +1,20 @@
 const request = require("supertest");
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
 const app = require("../src/app");
+
+let mongoServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri);
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongoServer.stop();
+});
 
 describe("Task API", () => {
   it("should create a task", async () => {
